fb8790fdfa3ff73108f3134361abe71285183197
nathan <rahm.nathan@gmail.com>
2019-11-27 19:13:45 -0600

    Extended exiting exception handler.

--- src/main/java/com/nathanrahm/fridge/exception/FridgeManagerExceptionHandler.java
+++ src/main/java/com/nathanrahm/fridge/exception/FridgeManagerExceptionHandler.java
@@ -7,9 +7,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class FridgeManagerExceptionHandler {
+public class FridgeManagerExceptionHandler extends ResponseEntityExceptionHandler {
     private final Logger logger = LoggerFactory.getLogger(FridgeManagerExceptionHandler.class);
     private final ObjectMapper MAPPER = new ObjectMapper();
 
